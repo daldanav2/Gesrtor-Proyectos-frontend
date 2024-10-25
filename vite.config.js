3b7413762dba7450aa4a3b6876deb5047af5474b
daldanav2 <darwinav@gmail.com>
2024-10-25 01:03:28 -0600

    tratar enrutar

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Otras configuraciones si las tienes
+  },
+  build: {
+    rollupOptions: {
+      input: '/index.html', // Asegúrate de que esté configurado correctamente
+    }
+  },
+  // Configuración para enrutamiento en Fly.io o cualquier otro servicio
+  optimizeDeps: {
+    include: ['react-router-dom']
+  },
+  ssr: false
 })
+
+
